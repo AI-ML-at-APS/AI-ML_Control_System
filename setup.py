e7e936aeeff9537ee873f951d27cf33939ed4a0c
lucarebuffi <lrebuffi@anl.gov>
2021-10-01 15:39:46 -0500

    bug fixing

---
+++ setup.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# #########################################################################
+# Copyright (c) 2020, UChicago Argonne, LLC. All rights reserved.         #
+#                                                                         #
+# Copyright 2020. UChicago Argonne, LLC. This software was produced       #
+# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
+# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
+# U.S. Department of Energy. The U.S. Government has rights to use,       #
+# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
+# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
+# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
+# modified to produce derivative works, such modified software should     #
+# be clearly marked, so as not to confuse it with the version available   #
+# from ANL.                                                               #
+#                                                                         #
+# Additionally, redistribution and use in source and binary forms, with   #
+# or without modification, are permitted provided that the following      #
+# conditions are met:                                                     #
+#                                                                         #
+#     * Redistributions of source code must retain the above copyright    #
+#       notice, this list of conditions and the following disclaimer.     #
+#                                                                         #
+#     * Redistributions in binary form must reproduce the above copyright #
+#       notice, this list of conditions and the following disclaimer in   #
+#       the documentation and/or other materials provided with the        #
+#       distribution.                                                     #
+#                                                                         #
+#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
+#       Laboratory, ANL, the U.S. Government, nor the names of its        #
+#       contributors may be used to endorse or promote products derived   #
+#       from this software without specific prior written permission.     #
+#                                                                         #
+# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
+# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
+# POSSIBILITY OF SUCH DAMAGE.                                             #
+# #########################################################################
+
+import os
+
+try:
+    from setuptools import find_packages, setup
+except AttributeError:
+    from setuptools import find_packages, setup
+
+NAME = 'ML-Control-System-34-ID-C'
+VERSION = '0.0.1'
+ISRELEASED = False
+
+DESCRIPTION = 'ML Control System for the Beamline 34-ID-C @ APS'
+README_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
+LONG_DESCRIPTION = open(README_FILE).read()
+AUTHOR = 'Luca Rebuffi'
+AUTHOR_EMAIL = 'lrebuffi@anl.gov'
+URL = 'https://github.com/APS-34-ID-C/ML_Control_System'
+DOWNLOAD_URL = 'https://github.com/APS-34-ID-C/ML_Control_System'
+LICENSE = 'GPLv3'
+
+KEYWORDS = (
+    'raytracing',
+    'simulator',
+    'machine learning'
+)
+
+CLASSIFIERS = (
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: X11 Applications :: Qt',
+    'Environment :: Console',
+    'Environment :: Plugins',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Science/Research',
+)
+
+SETUP_REQUIRES = (
+    'setuptools',
+)
+
+INSTALL_REQUIRES = (
+    'OASYS1-ShadowOui>=1.5.131',
+    'OASYS1-ShadowOui-Advanced-Tools>=1.0.82',
+    'OASYS1-ML>=0.0.1',
+)
+
+PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
+
+PACKAGE_DATA = {}
+NAMESPACE_PACAKGES = []
+ENTRY_POINTS = {}
+
+if __name__ == '__main__':
+     setup(
+          name = NAME,
+          version = VERSION,
+          description = DESCRIPTION,
+          long_description = LONG_DESCRIPTION,
+          author = AUTHOR,
+          author_email = AUTHOR_EMAIL,
+          url = URL,
+          download_url = DOWNLOAD_URL,
+          license = LICENSE,
+          keywords = KEYWORDS,
+          classifiers = CLASSIFIERS,
+          packages = PACKAGES,
+          package_data = PACKAGE_DATA,
+          setup_requires = SETUP_REQUIRES,
+          install_requires = INSTALL_REQUIRES,
+          entry_points = ENTRY_POINTS,
+          namespace_packages=NAMESPACE_PACAKGES,
+          include_package_data = True,
+          zip_safe = False,
+          )
