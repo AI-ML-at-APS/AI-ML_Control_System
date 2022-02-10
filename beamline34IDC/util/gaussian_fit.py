5b38283618add4b666216aaec530151581ee9719
saugat <saugat.kandel@u.northwestern.edu>
2022-02-10 14:01:18 -0600

    Added differential evolution to the gaussian fit. It might be more robust but mroe time consuming than the curve_fit method.

--- beamline34IDC/util/gaussian_fit.py
+++ beamline34IDC/util/gaussian_fit.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.optimize import curve_fit
+from scipy.optimize import differential_evolution
+
 
 def generalized_2D_gaussian(xdata_tuple: np.ndarray,  # array consisting of 2d points.
                             amplitude: float,
@@ -44,7 +47,6 @@ def calculate_gaussian_fit(data_2D: np.ndarray, x: np.ndarray = None, y: np.ndar
     ----------
     .. [1] https://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
     """
-    from scipy.optimize import curve_fit
 
     if x is None and y is None:
         nx = data_2D.shape[-1]
@@ -57,6 +59,65 @@ def calculate_gaussian_fit(data_2D: np.ndarray, x: np.ndarray = None, y: np.ndar
     bounds_min = [0, x[0], y[0], 0, 0, -np.pi / 4, 0]
     bounds_max = [data_2D.sum(), x[-1], y[-1], x[-1] * 2, y[-1] * 2, np.pi / 4, data_2D.max()]
     popt, _ = curve_fit(generalized_2D_gaussian,
+                        xdata,
+                        data_2D.flatten(),
+                        bounds=[bounds_min, bounds_max])
+    amplitude, center_x, center_y, sigma_x, sigma_y, theta, offset = popt
+    gaussian_fit = {"amplitude": amplitude,
+                    "center_x": center_x,
+                    "center_y": center_y,
+                    "sigma_x": sigma_x,
+                    "fwhm_x": 2.355 * sigma_x,
+                    "sigma_y": sigma_y,
+                    "fwhm_y": 2.355 * sigma_y,
+                    "theta": theta,
+                    "offset": offset}
+    return gaussian_fit
+
+def differential_evolution_gaussian_fit(data_2D: np.ndarray, x: np.ndarray = None, y: np.ndarray = None) -> dict:
+    r"""Fit a 2-d gaussian to the probe intensities (not amplitudes) and return the fit parameters.
+
+    The returned dictionary contains the following fit parameters (as described in [1]_):
+        * ``amplitude`` : Amplitude of the fitted gaussian.
+        * ``center_x`` : X-offset of the center of the fitted gaussian.
+        * ``center_y`` : Y-offset of the center of the fitted gaussian.
+        * ``theta`` : Clockwise rotation angle for the gaussian fit. Prior to the rotation, primary axes of the \
+          | gaussian  are aligned to the X and Y axes.
+        * ``sigma_x`` : Spread (standard deviation) of the gaussian fit along the x-axis (prior to rotation).
+        * ``fwhm_x`` : FWHM along x-axis (prior to rotation).
+        * ``sigma_y`` : Spread of the gaussian fit along the y-axis (prior to rotation).
+        * ``fwhm_y`` : FWHM along y-axis (before rotation).
+        * ``offset`` : Constant level of offset applied to the fit.
+
+    Returns
+    -------
+    out : dict
+        Dictionary containing the fit parameters.
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
+    """
+
+    def squared_loss(parameters):
+        val = generalized_2D_gaussian(xdata, *parameters)
+        return np.sum((data_2D.flatten() - val)**2)
+
+
+    if x is None and y is None:
+        nx = data_2D.shape[-1]
+        ny = data_2D.shape[-2]
+        y = np.arange(-ny // 2, ny // 2)
+        x = np.arange(-ny // 2, nx // 2)
+
+    yy, xx = np.meshgrid(y, x)
+    xdata = np.stack((xx.flatten(), yy.flatten()), axis=1)
+    bounds_min = [0, x[0], y[0], 0, 0, -np.pi / 4, 0]
+    bounds_max = [data_2D.sum(), x[-1], y[-1], x[-1] * 2, y[-1] * 2, np.pi / 4, data_2D.max()]
+    bounds_min = np.array(bounds_min) + 1e-7
+    bounds = np.array([bounds_min, bounds_max]).T
+
+    result = differential_evolution(generalized_2D_gaussian,
                         xdata,
                         data_2D.flatten(),
                         bounds=[bounds_min, bounds_max])
--- work_directory/focus_optimization/nelder_mead_simplex/vkb_motor_4.py
+++ work_directory/focus_optimization/nelder_mead_simplex/vkb_motor_4.py
@@ -1,15 +1,15 @@
 #%%
 import os
-import sys
-sys.path.append('/Users/saugat/code/oasys/ML_Control_System/work_directory/focus_optimization/nelder_mead_simplex')
+from beamline34IDC.simulation.facade import Implementors
+from beamline34IDC.simulation.facade.focusing_optics_factory import focusing_optics_factory_method
+from beamline34IDC.simulation.facade.focusing_optics_interface import Movement
 
-from beamline34IDC.simulation.focusing_optics_system import FocusingOpticsSystem, Movement
-from beamline34IDC.util.common import PreProcessorFiles, plot_shadow_beam_spatial_distribution, \
-    load_shadow_beam, get_shadow_beam_spatial_distribution
+from beamline34IDC.util.shadow.common import \
+    plot_shadow_beam_spatial_distribution, get_shadow_beam_spatial_distribution,\
+    load_shadow_beam, PreProcessorFiles
 from beamline34IDC.util import clean_up
 import matplotlib.pyplot as plt
 
-from gaussian_fit import calculate_gaussian_fit
 import scipy
 
 #%%
@@ -24,10 +24,52 @@ def loss_function(focusing_system, trans, move_type = Movement.RELATIVE):
     _, peak = getPeakIntensity(focusing_system)
     return peak
 
-def getAmplitudeFromGaussianFit(focusing_system):
-    out_beam = focusing_system.get_beam()
-    hist, dw = get_shadow_beam_spatial_distribution(out_beam)
-    pass
+def reinitialize():
+    focusing_system = focusing_optics_factory_method(implementor=Implementors.SHADOW)
+
+    focusing_system.initialize(input_photon_beam=input_beam,
+                               rewrite_preprocessor_files=PreProcessorFiles.NO,
+                               rewrite_height_error_profile_files=False)
+    output_beam = focusing_system.get_photon_beam(random_seed=101, remove_lost_rays=True)
+    return focusing_system, output_beam
+
+def runs(focusing_system, n_runs=5, translation=None,
+         translation_type=Movement.RELATIVE,
+         fig_save_fname=None):
+    hists_all = []
+    dws_all = []
+
+    n_rows = n_runs // 5 + 1
+    n_cols = 5
+
+    fig1, axes1 = plt.subplots(n_rows, n_cols, figsize=[10, 2 * n_rows])
+    #fig2, axes2 = plt.subplot()
+
+    for i in range(n_runs):
+        if translation is not None:
+            focusing_system.move_vkb_motor_4_translation(translation, translation_type)
+        out_beam = focusing_system.get_photon_beam(remove_lost_rays=True)
+        hist, dw = get_shadow_beam_spatial_distribution(out_beam)
+        hists_all.append(hist)
+        dws_all.append(dw)
+
+        ax = axes1[i // 5, i % 5]
+        ax.pcolormesh(hist. hh, hist.vv, hist.data_2D)
+        ax.set_aspect('equal')
+
+        #h_sigma = ticket['sigma_h'],
+        #h_fwhm = ticket['fwhm_h'],
+        #h_centroid = ticket['centroid_h'],
+        #v_sigma = ticket['sigma_v'],
+        #v_fwhm = ticket['fwhm_v'],
+        #v_centroid = ticket['centroid_v'],
+        #integral_intensity = integral_intensity,
+        #peak_intensity = peak_intensity
+    fig1.tight_layout()
+    if fig_save_fname is not None:
+        fig1.savefig(fig_save_fname, bbox_inches='tight')
+    plt.show(block=True)
+    return hists_all, dws_all
 
 #%%
 work_dir = '/Users/saugat/code/oasys/ML_Control_System/work_directory'
@@ -37,18 +79,14 @@ input_beam = load_shadow_beam("primary_optics_system_beam.dat")
 #%%
 # Focusing Optics System -------------------------
 
-focusing_system = FocusingOpticsSystem()
+focusing_system, out_beam = reinitialize()
 
-focusing_system.initialize(input_beam=input_beam,
-                           rewrite_preprocessor_files=PreProcessorFiles.NO,
-                           rewrite_height_error_profile_files=False)
 #%%
-out_beam = focusing_system.get_beam()
 plot_shadow_beam_spatial_distribution(out_beam)
 plt.show(block=True)
 
 #%%
-hist, dw = get_shadow_beam_spatial_distribution(out_beam)
+hist, dw = get_shadow_beam_spatial_distribution(out_beam, do_gaussian_fit=True)
 
 #%%
 print(getPeakIntensity(focusing_system))
