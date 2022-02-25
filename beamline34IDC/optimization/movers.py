88b0af27a357f797754d2b1eace3c17ec1597d1a
saugat <saugat.kandel@u.northwestern.edu>
2022-02-25 14:54:38 -0600

    Added function to get absolute positions from multiple motors at once.

--- beamline34IDC/optimization/movers.py
+++ beamline34IDC/optimization/movers.py
@@ -23,6 +23,7 @@ def get_motor_move_fn(focusing_system, motor):
         return motor
     raise ValueError
 
+
 def move_motors(focusing_system, motors, translations, movement='relative'):
     movement = get_movement(movement)
     if np.ndim(motors) == 0:
@@ -33,3 +34,29 @@ def move_motors(focusing_system, motors, translations, movement='relative'):
         motor_move_fn = get_motor_move_fn(focusing_system, motor)
         motor_move_fn(trans, movement=movement)
     return focusing_system
+
+
+def get_motor_absolute_position_fn(focusing_system, motor):
+    motor_get_pos_fns = {'hkb_4': focusing_system.get_hkb_motor_4_translation,
+                         'hkb_3': focusing_system.get_hkb_motor_3_pitch,
+                         'hkb_q': focusing_system.get_hkb_q_distance,
+                         'vkb_4': focusing_system.get_vkb_motor_4_translation,
+                         'vkb_3': focusing_system.get_vkb_motor_3_pitch,
+                         'vkb_q': focusing_system.get_vkb_q_distance}
+    if motor in motor_get_pos_fns:
+        return motor_get_pos_fns[motor]
+    if motor in motor_get_pos_fns.values():
+        return motor
+    raise ValueError
+
+
+def get_absolute_positions(focusing_system, motors):
+
+    if np.ndim(motors) == 0:
+        motors = [motors]
+
+    positions = []
+    for motor in motors:
+        position = get_motor_absolute_position_fn(focusing_system, motor)()
+        positions.append(position)
+    return positions
