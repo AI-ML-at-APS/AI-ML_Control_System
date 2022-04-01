cb71baa44ef7da57e58db3da6e83afb132b4ef35
lucarebuffi <lrebuffi@anl.gov>
2022-04-01 13:54:48 -0500

    Initialization of motors: added a proper file ini manager (from WavePy 2)

--- beamline34IDC/__init__.py
+++ beamline34IDC/__init__.py
@@ -44,3 +44,46 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
 # POSSIBILITY OF SUCH DAMAGE.                                             #
 # ----------------------------------------------------------------------- #
+
+
+###################################################################
+# DO NOT TOUCH THIS CODE -- BEGIN
+###################################################################
+import threading
+
+def synchronized_method(method):
+
+    outer_lock = threading.Lock()
+    lock_name = "__"+method.__name__+"_lock"+"__"
+
+    def sync_method(self, *args, **kws):
+        with outer_lock:
+            if not hasattr(self, lock_name): setattr(self, lock_name, threading.Lock())
+            lock = getattr(self, lock_name)
+            with lock:
+                return method(self, *args, **kws)
+
+    return sync_method
+
+class Singleton:
+    def __init__(self, decorated):
+        self.__decorated = decorated
+
+    @synchronized_method
+    def Instance(self, **args):
+        try:
+            return self.__instance
+        except AttributeError:
+            self.__instance = self.__decorated(**args)
+            return self.__instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `Instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self.__decorated)
+
+###################################################################
+# DO NOT TOUCH THIS CODE -- END
+###################################################################
+
