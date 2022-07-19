62276e63edf0131007bbbc12c92476fce57d00fc
lucarebuffi <lrebuffi@anl.gov>
2022-07-19 15:36:01 -0500

    data analysis

--- data_analysis/data_analyzer.py
+++ data_analysis/data_analyzer.py
@@ -2,12 +2,15 @@ import os, glob
 
 import numpy
 
-from single_shot_data import extract_shape_from_measurement_file, DataSet
+from single_shot_data import extract_shape_from_measurement_file
 
-home_dir = "/Users/lrebuffi/Library/CloudStorage/Box-Box/Luca_Documents/Simulations/APS/34-ID-C/Beamtime/Jul14-2022/bender_calibration"
+root_dir = "/Users/lrebuffi/Library/CloudStorage/Box-Box/Luca_Documents"
+if not os.path.exists(root_dir): root_dir = "/Users/lrebuffi/Box Sync/Luca_Documents"
+
+home_dir = os.path.join(root_dir, "Simulations/APS/34-ID-C/Beamtime/Jul14-2022/bender_calibration")
 
 directories = {"horizontal" : [os.path.join(home_dir, "slits_full", "horizontal"), os.path.join(home_dir, "slits_150_150", "horizontal")],
-               "vertical"   : [os.path.join(home_dir, "slits_full", "vertical"), os.path.join(home_dir, "slits_150_150", "vertical")]}
+               "vertical"   : [os.path.join(home_dir, "slits_full", "vertical"),   os.path.join(home_dir, "slits_150_150", "vertical")]}
 
 dataset   = {"horizontal" : "line_curve_x", "vertical" : "line_curve_y"}
 distances =  {"horizontal" : 387, "vertical" : 493}
@@ -50,37 +53,58 @@ def process_direction(direction):
 
     return [upward_data_full, upward_data_150], [downward_data_full, downward_data_150]
 
+def plot_axis(ax, data, title, degree, ylim, escluded=[]):
+    ax.plot(data[:, 0], data[:, 1], "bo")
+    text = ""
+    xx = numpy.ma.array(data[:, 0], mask=False)
+    yy = numpy.ma.array(data[:, 1], mask=False)
+
+    if len(escluded) > 0:
+        xx.mask[escluded] = True
+        yy.mask[escluded] = True
+
+    xx = xx.compressed()
+    yy = yy.compressed()
+    param = numpy.polyfit(x=xx, y=yy, deg=degree)
+    for i in range(len(param)): text += "p" + str(i) + "=" +str(round(param[i], 6)) + "\n"
+    text += "\n"
+    ax.plot(xx, numpy.poly1d(param)(xx))
+
+    ax.text(x=ax.get_xlim()[0] * 1.001, y=ylim, s=text, fontsize=8)
+    ax.set_title(title)
 
 horizontal_upward, horizontal_downward = process_direction(direction="horizontal")
 vertical_upward, vertical_downward     = process_direction(direction="vertical")
 
+fig, axes = plt.subplots(2, 4, figsize=(16, 8))
+fig.suptitle("Bender Calibration")
 
-def plot_axis(ax, data, title, degree, start=[0], end=[-1]):
-    ax.plot(data[:, 0], data[:, 1], "bo")
-    text = ""
-    for deg in range(degree):
-        s = start[deg]
-        e = end[deg] if end[deg] != -1 else len(data[:, 0])
-        param = numpy.polyfit(x=data[s:e, 0], y=data[s:e, 1], deg=deg+1)
-        for i in range(len(param)): text += "p" + str(i) + "=" +str(round(param[i], 6)) + "\n"
-        text += "\n"
-        ax.plot(data[s:e, 0], numpy.poly1d(param)(data[s:e, 0]))
-
-    ax.text(x=ax.get_xlim()[0] * 1.001, y=ax.get_ylim()[1] * (0.96-0.08*(degree-1)), s=text, fontsize=8)
-    ax.set_title(title)
-
+plot_axis(axes[0, 0], horizontal_upward[0],   "Horizontal, slits full: upward",   1, 120)
+plot_axis(axes[0, 1], horizontal_downward[0], "Horizontal, slits full: downward", 1, 120)
+plot_axis(axes[1, 0], horizontal_upward[1],   "Horizontal, slits 150: upward",    1, 120)
+plot_axis(axes[1, 1], horizontal_downward[1], "Horizontal, slits 150: downward",  1, 120)
+plot_axis(axes[0, 2], vertical_upward[0],     "Vertical, slits full: upward",     1, 228)
+plot_axis(axes[0, 3], vertical_downward[0],   "Vertical, slits full: downward",   1, 228)
+plot_axis(axes[1, 2], vertical_upward[1],     "Vertical, slits 150: upward",      1, 228)
+plot_axis(axes[1, 3], vertical_downward[1],   "Vertical, slits 150: downward",    1, 228)
 
 fig, axes = plt.subplots(2, 4, figsize=(16, 8))
 fig.suptitle("Bender Calibration")
 
-plot_axis(axes[0, 0], horizontal_upward[0], "Horizontal, slits full: upward", 1)
-plot_axis(axes[0, 1], horizontal_downward[0], "Horizontal, slits full: downward", 1)
-plot_axis(axes[1, 0], horizontal_upward[1], "Horizontal, slits 150: upward", 2, start=[4, 0], end=[-4, -1])
-plot_axis(axes[1, 1], horizontal_downward[1], "Horizontal, slits 150: downward", 2, start=[4, 1], end=[-4, -1])
-plot_axis(axes[0, 2], vertical_upward[0],   "Vertical, slits full: upward", 1)
-plot_axis(axes[0, 3], vertical_downward[0], "Vertical, slits full: downward", 1)
-plot_axis(axes[1, 2], vertical_upward[1],   "Vertical, slits 150: upward", 2, start=[4, 2], end=[-4, -1])
-plot_axis(axes[1, 3], vertical_downward[1], "Vertical, slits 150: downward", 2, start=[4, 2], end=[-4, -1])
+directories = {"horizontal" : [os.path.join(home_dir, "slits_full", "horizontal_2"), os.path.join(home_dir, "slits_150_150", "horizontal_2")],
+               "vertical"   : [os.path.join(home_dir, "slits_full", "vertical_2"),   os.path.join(home_dir, "slits_150_150", "vertical_2")]}
+
+horizontal_upward, horizontal_downward = process_direction(direction="horizontal")
+vertical_upward, vertical_downward     = process_direction(direction="vertical")
+
+plot_axis(axes[0, 0], horizontal_upward[0],   "Horizontal, slits full: upward",   2, 115, escluded=[0])
+plot_axis(axes[0, 1], horizontal_downward[0], "Horizontal, slits full: downward", 2, 115, escluded=[0, 1, 10])
+plot_axis(axes[1, 0], horizontal_upward[1],   "Horizontal, slits 150: upward",    2, 115, escluded=[])
+plot_axis(axes[1, 1], horizontal_downward[1], "Horizontal, slits 150: downward",  2, 115, escluded=[])
+plot_axis(axes[0, 2], vertical_upward[0],     "Vertical, slits full: upward",     2, 220, escluded=[])
+plot_axis(axes[0, 3], vertical_downward[0],   "Vertical, slits full: downward",   2, 220, escluded=[0, 1, 2])
+plot_axis(axes[1, 2], vertical_upward[1],     "Vertical, slits 150: upward",      2, 220, escluded=[0, 1])
+plot_axis(axes[1, 3], vertical_downward[1],   "Vertical, slits 150: downward",    2, 220, escluded=[1])
 
 plt.show()
 
